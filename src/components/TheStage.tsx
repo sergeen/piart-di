eb209aa7ace43b595d25afe35c62ee88a636ec02
Sergio Scotta <sergeen@gmail.com>
2023-11-19 20:48:46 -0300

    Germination: now I can save and retrive drawings from the local store

--- src/components/TheStage.tsx
+++ src/components/TheStage.tsx
@@ -1,4 +1,4 @@
-import { Component, createSignal, For } from "solid-js";
+import { Component, createEffect, createSignal, For, onMount } from "solid-js";
 import {
   coordStringToCoordObj,
   coordObjToCoordStr,
@@ -20,14 +20,9 @@ declare module "solid-js" {
   }
 }
 
-/**
- * TODO: la forma en que está hecho el componente implica que si las dimensiones
- * de la matriz cambia, se va a romper el dibujo.
- */
-
 export const initialState = {
-  defaultColor: "transparent",
-  colorPaletes: [
+  currentColor: "transparent",
+  currentPalette: [
     {
       name: "PAPERBACK-2",
       url: "https://lospec.com/palette-list/paperback-2",
@@ -35,19 +30,24 @@ export const initialState = {
       colors: ["#b8c2b9", "#382b26", "transparent"],
     },
   ],
+  localStoredDrawings: JSON.parse(localStorage.getItem('svgDrawerStoredDrawings')),
+  currentMatrix: createMatrix(16),
+  currentDrawingTitle: "Untitled",
+  currentDrawing: {
+    title: "",
+    date: null,
+    matrix: []
+  }
 };
 
 const TheStage: Component<matrix> = () => {
-  const matrix = createMatrix(16);
 
-  const [currentDrawingName, setCurrentDrawingName] = createSignal("untitled");
-  const [currentMatrix, setCurrentMatrix] = createSignal(matrix);
-  const [currentColor, setCurrentColor] = createSignal(
-    initialState.defaultColor
-  );
-  const [currentPalette, setCurrentPalette] = createSignal(
-    initialState.colorPaletes[0].colors //TODO: como acceso a esto desde currentPalette().colors ?
-  );
+  const [currentMatrix, setCurrentMatrix] = createSignal(initialState.currentMatrix);
+  const [currentColor, setCurrentColor] = createSignal(initialState.currentColor);
+  const [currentPalette, setCurrentPalette] = createSignal(initialState.currentPalette[0].colors);
+  const [currentDrawingTitle, setCurrentDrawingTitle] = createSignal(initialState.currentDrawingTitle)
+  const [currentDrawing, setCurrentDrawing] = createSignal(initialState.currentDrawing)
+  const [currentLocalStoredDrawings, setCurrentLocalStoredDrawings] = createSignal(initialState.localStoredDrawings)
 
   const editSingleCell = (coordString: string) => {
     const coordinates = coordStringToCoordObj(coordString);
@@ -56,8 +56,46 @@ const TheStage: Component<matrix> = () => {
     setCurrentMatrix([...newMatrix]);
   };
 
+  const pushToArray = (object, array) => {
+    const index = array.findIndex((obj) => obj.title === object.title);
+    if (index !== -1) {
+      array[index] = currentDrawing();
+    } else {
+      array.push(currentDrawing());
+    }
+    return array
+  };
+
+  createEffect(() => {
+
+    // When any of this values are updated
+    currentDrawingTitle();
+    currentColor();
+    currentMatrix();
+    currentPalette();
+
+    // Set the current Drawing
+    setCurrentDrawing({
+      title: currentDrawingTitle(),
+      date: new Date(),
+      matrix: currentMatrix(),
+    })
+
+    return () => {
+      console.log("cleanup");
+    }
+
+  })
+
   const handleSaveToLocalStorage = () => {
-    localStorage.setItem(`svgDrawerMatrix_${currentDrawingName()}`, JSON.stringify(currentMatrix()));
+    if (currentLocalStoredDrawings() === null ) {
+      localStorage.setItem("svgDrawerStoredDrawings", JSON.stringify([currentDrawing()]))
+    } else {
+      localStorage.setItem("svgDrawerStoredDrawings", JSON.stringify(pushToArray(
+        currentDrawing(), 
+        currentLocalStoredDrawings()
+      )))
+    }
   };
 
   return (
@@ -65,14 +103,15 @@ const TheStage: Component<matrix> = () => {
       <div class="action-bar">
         <input
           type="text"
-          onchange={(e) => setCurrentDrawingName(e.target.value)}
-          value={currentDrawingName()}
+          onchange={(e) => setCurrentDrawingTitle(e.target.value)}
+          value={currentDrawingTitle()}
           class="title-input"
         />
-        <button onclick={ e => handleSaveToLocalStorage()}>
+        <button onclick={(e) => handleSaveToLocalStorage()}>
           {TextContent.saveButton}
         </button>
       </div>
+
       <div class="color-selector">
         <p>{TextContent.colorSelectorTitle}</p>
         <div class="color-samples">
@@ -93,8 +132,9 @@ const TheStage: Component<matrix> = () => {
           </For>
         </div>
       </div>
+
       <div class="stage">
-        {matrix.map((elementX, xIndex) => (
+        {currentMatrix().map((elementX, xIndex) => (
           <div class="rowDiv">
             {elementX.map((elementX, yIndex) => (
               <div
